8cbcc017b55d2e19d8f1dde5d5a6485289ada5ef
Jacob <jsome635@hawaii.edu>
2018-02-22 18:40:37 -1000

    Reformatted all given files.

--- src/main.c
+++ src/main.c
@@ -13,54 +13,51 @@
 #include "host.h"
 
 
-void main()
-{
+void main() {
 
-pid_t pid;  /* Process id */
-int k=0;
-int status;
-struct net_node *node_list;
-struct net_node *p_node;
+    pid_t pid;  /* Process id */
+    int k = 0;
+    int status;
+    struct net_node *node_list;
+    struct net_node *p_node;
 
 /*
  * Read network configuration file, which specifies
  *   - nodes, creates a list of nodes
  *   - links, creates/implements the links, e.g., using pipes or sockets
  */
-net_init();
-node_list = net_get_node_list(); /* Returns the list of nodes */
+    net_init();
+    node_list = net_get_node_list(); /* Returns the list of nodes */
 
 
 /* Create nodes, which are child processwa */
 
-for (p_node = node_list; p_node != NULL; p_node = p_node->next) {
+    for (p_node = node_list; p_node != NULL; p_node = p_node->next) {
 
-	pid = fork();
+        pid = fork();
 
-	if (pid == -1) {
-		printf("Error:  the fork() failed\n");
-		return;
-	}
-	else if (pid == 0) { /* The child process, which is a node  */
-		if (p_node->type == HOST) {  /* Execute host routine */
-			host_main(p_node->id);
-		}
-		else if (p_node->type = SWITCH) {
-			/* Execute switch routine, which you have to write */
-		}
-		return;
-	}
-}
+        if (pid == -1) {
+            printf("Error:  the fork() failed\n");
+            return;
+        } else if (pid == 0) { /* The child process, which is a node  */
+            if (p_node->type == HOST) {  /* Execute host routine */
+                host_main(p_node->id);
+            } else if (p_node->type = SWITCH) {
+                /* Execute switch routine, which you have to write */
+            }
+            return;
+        }
+    }
 
 /*
  * Parent process: Execute manager routine.
  */
-man_main();
+    man_main();
 
 
 /*
  * We reach here if the user quits the manager.
  * The following will terminate all the children processes.
  */
-kill(0, SIGKILL); /* Kill all processes */
+    kill(0, SIGKILL); /* Kill all processes */
 }
